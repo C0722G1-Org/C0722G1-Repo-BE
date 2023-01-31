d5c1eace0bdc498ae24814fc391998e33e22f1ea
DanHuy77 <b77cwalk@gmail.com>
2023-01-31 12:46:12 +0700

    [HuyDN] [Update method findPostById to get a Post detail]

--- C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/repository/post/IPostRepository.java
+++ C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/repository/post/IPostRepository.java
@@ -15,23 +15,52 @@ public interface IPostRepository extends JpaRepository<Post, Long> {
      * Created by: HuyDN
      * Created date: 31/01/2023
      * Catching NullPointerException
-     * @param id:  a Post' id
+     *
+     * @param id: a Post' id
      * @return a Post object that can be showed on Post detail screen
      */
-    @Query(value = "select name_post, " +
-            "area, " +
-            "note_post, " +
-            "description_post, " +
-            "price, " +
-            "approval, " +
-            "date_creation, " +
-            "direction, " +
-            "status_post, " +
-            "address, " +
-            "demand_type, " +
-            "land_type, " +
-            "image_list," +
-            "customer from post where post.id_post = %:id% and flag_deleted = false "
+    @Query(value = "select post.name_post, " +
+            "post.area, " +
+            "post.note_post, " +
+            "post.description_post, " +
+            "post.price, " +
+            "post.approval, " +
+            "post.date_creation, " +
+            "direction.name, " +
+            "status_post.nam_status_post, " +
+            "address.number_address, " +
+            "demand_type.name_demand_type, " +
+            "land_type.name_land_type, " +
+            "image_list.url," +
+            "customer.name, customer.email_customer from post join direction " +
+            "on post.direction_id_direction = direction.id_direction join status_post " +
+            "on post.status_post_id_status_post = status_post.id_status_post join address " +
+            "on post.address_id_address = address.id_address join demand_type " +
+            "on post.demand_type_id_demand_type = demand_type.id_demand_type join image_list " +
+            "on post.image_list_id_image_list = image_list.id_image_list join customer " +
+            "on post.id_customer = customer.id_customer" +
+            "where post.id_post = %:id% and flag_deleted = false",
+            countQuery = "select post.name_post," +
+                    "            post.area," +
+                    "            post.note_post," +
+                    "            post.description_post," +
+                    "            post.price," +
+                    "            post.approval," +
+                    "            post.date_creation," +
+                    "            direction.name," +
+                    "            status_post.nam_status_post," +
+                    "            address.number_address," +
+                    "            demand_type.name_demand_type," +
+                    "            land_type.name_land_type," +
+                    "            image_list.url," +
+                    "            customer.name, customer.email_customer from post join direction" +
+                    "            on post.direction_id_direction = direction.id_direction join status_post" +
+                    "            on post.status_post_id_status_post = status_post.id_status_post join address" +
+                    "            on post.address_id_address = address.id_address join demand_type" +
+                    "            on post.demand_type_id_demand_type = demand_type.id_demand_type join image_list" +
+                    "            on post.image_list_id_image_list = image_list.id_image_list join customer" +
+                    "            on post.id_customer = customer.id_customer" +
+                    "            where post.id_post = %:id% and flag_deleted = false"
             , nativeQuery = true)
     Post findPostById(@Param("id") Long id);
 }
