77f385406ab08b6dc0ab7f23b4fca166729ea5bc
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2023-01-31 09:36:15 +0700

    update

--- C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/entity/customer/Customer.java
+++ C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/entity/customer/Customer.java
@@ -19,8 +19,6 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCustomer;
     private String nameCustomer;
-    @OneToMany(mappedBy = "customer")
-    private Set<PhoneCustomer> phoneCustomerSet;
     private String emailCustomer;
     private String addressCustomer;
     private String idCardCustomer;
@@ -28,6 +26,8 @@ public class Customer {
     private Integer genderCustomer;
     private boolean flagDelete = false;
     private int approvalCustomer;
+    private String phoneCustomer1;
+    private String phoneCustomer2;
     @OneToOne
     private Account account;
 }
