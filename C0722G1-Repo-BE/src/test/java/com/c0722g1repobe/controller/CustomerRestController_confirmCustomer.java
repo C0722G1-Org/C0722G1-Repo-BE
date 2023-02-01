95241718aeededc39c32f4f24b9ff3d9cd5feed9
unknown <you@example.com>
2023-02-01 22:08:06 +0700

    HocH create JUnit5 5.5.1 display customer list

--- C0722G1-Repo-BE/src/test/java/com/c0722g1repobe/controller/CustomerRestController_confirmCustomer.java
+++ C0722G1-Repo-BE/src/test/java/com/c0722g1repobe/controller/CustomerRestController_confirmCustomer.java
@@ -15,7 +15,7 @@ public class CustomerRestController_confirmCustomer {
 
     @Test
     /**
-     * Note: Sử dụng MockMvcRequestBuilders.delete cho phương thức cập nhật 1 record column:
+     * Note: Sử dụng MockMvcRequestBuilders.delete cho phương thức cập nhật 1 record(column, attributes) vì:
      *       Tính chất của phương thức confirmCustomer tương tự với phương thức dùng để xóa mềm, có tác dụng
      *          cập nhật dữ liệu của column(attributes: thuộc tính) của confirmCustomer.
      *
