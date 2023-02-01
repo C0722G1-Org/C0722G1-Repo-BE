635c425b1e535ba8d5a4bd108cff50f86341495b
bom <bom@gmail.com>
2023-02-01 14:47:24 +0700

    UyDD add new source code for function 5.5.4 display list post

--- C0722G1-Repo-BE/C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/repository/post/IPostRepository.java
+++ C0722G1-Repo-BE/C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/repository/post/IPostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -22,8 +23,8 @@ public interface IPostRepository extends JpaRepository<Post, Long> {
             " join demand_type on post.id_post = demand_type.id_demand_type" +
             " join land_type on post.id_post = land_type.id_land_type" +
             " join image_list on post.id_post = image_list.id_image_list" +
-            " join customer on post.id_customer = customer.id_customer" +
+            " join customer on post.customer_id_customer = customer.id_customer" +
             " join account on customer.account_id_account = account.id_account" +
-            " where account.username_account = 'khachhang123'", nativeQuery = true)
-    Page<Post> findAllPost(Pageable pageable, String userNameAccount);
+            " where account.username_account like :userNameAccount", nativeQuery = true)
+    Page<Post> findAllPost(Pageable pageable,@Param("userNameAccount") String userNameAccount);
 }
