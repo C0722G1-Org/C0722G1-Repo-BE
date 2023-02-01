d0f57e782a4d1d1a5fa9ffb002dbb8e6b3aebab0
TranGia <quocdat.tran453@gmail.com>
2023-02-01 20:40:19 +0700

    test

--- C0722G1-Repo-BE/src/test/java/com/c0722g1repobe/controller/PostRestController_searchListPost.java
+++ C0722G1-Repo-BE/src/test/java/com/c0722g1repobe/controller/PostRestController_searchListPost.java
@@ -71,7 +71,7 @@ public class PostRestController_searchListPost {
     }
 
     /**
-     * Function used to test the search list of Posts with parameter does exist in DB, list has size > 0.Http.OK
+     * Function used to test the search list of Posts with parameter does exist in DB, list has size > 0
      * Time: 01/02/2023
      * Author:DatTQ
      */
