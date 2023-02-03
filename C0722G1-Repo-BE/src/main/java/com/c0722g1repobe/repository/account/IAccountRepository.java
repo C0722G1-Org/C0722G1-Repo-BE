1acfe2f3226d062930581bd53e168516bbb06a11
PhuocSang1902 <110214702+PhuocSang1902@users.noreply.github.com>
2023-02-03 23:00:21 +0700

    SangNP update code from dev

--- C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/repository/account/IAccountRepository.java
+++ C0722G1-Repo-BE/src/main/java/com/c0722g1repobe/repository/account/IAccountRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 public interface IAccountRepository extends JpaRepository<Account, Long> {
 
@@ -37,4 +39,47 @@ public interface IAccountRepository extends JpaRepository<Account, Long> {
 
     @Query(value = "select * from account where idAccount =:idAccount and flag_delete = 0", nativeQuery = true)
     Account findByIdAccount(@Param(value = "idAccount") Long idAccount);
+
+    /**
+     * Create by LongPT
+     * Date created 31/1/2023
+     * Function save account
+     */
+    @Query(value = "insert into account(username_account, encrypt_password) values (:username, :password)", nativeQuery = true)
+    public void saveAccount(@Param("username") String username, @Param("password") String password);
+     * Create by: PhuongLTH,
+     * Date created: 31/01/2023,
+     * Function: findByUsername,existsByUsername,existsByEmail
+     * @param usernameAccount,email
+     * @return HttpStatus.OK if have usernameAccount and email in database or HttpStatus.NOT_FOUND if id not found in database
+     */
+
+    @Query(value = "select id_account, email, encrypt_password, name, username_account,flag_delete from account where flag_delete = false and username_account = :username_account",
+            countQuery = "select id_account, email, encrypt_password, name, username_account,flag_delete from account where flag_delete = false and username_account = :username_account",
+            nativeQuery = true)
+    Optional<Account> findByUsername(@Param("username_account") String usernameAccount);
+
+    /**
+     * Create by: PhuongLTH,
+     * Date created: 31/01/2023,
+     * Function: existsByUsername
+     * @param usernameAccount,email
+     * @return HttpStatus.OK if have usernameAccount and email in database or HttpStatus.NOT_FOUND if id not found in database
+     */
+    @Query(value = "select username_account from account where username_account = :username_account",
+            countQuery = "select username_account from account where username_account = :username_account",
+            nativeQuery = true)
+    Boolean existsByUsername(@Param("username_account")String usernameAccount);
+
+    /**
+     * Create by: PhuongLTH,
+     * Date created: 31/01/2023,
+     * Function: findByUsername,existsByUsername,existsByEmail
+     * @param email
+     * @return HttpStatus.OK if have usernameAccount and email in database or HttpStatus.NOT_FOUND if id not found in database
+     */
+    @Query(value = "select email from account where email = :?",
+            countQuery = "select email from account where email = :?",
+            nativeQuery = true)
+    Boolean existsByEmail(String email);
 }
